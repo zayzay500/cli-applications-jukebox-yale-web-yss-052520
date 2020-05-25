568a2020bb5ee4b947f27f49c819fbe49464d925
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-25 01:25:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,7 +20,20 @@ def help
   puts "- exit : exits this program"
 end
 
-def play
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  
+  songs.each_with_index do |song, index|
+    if input == song
+      puts "Playing #{song}"
+    elsif input == index + 1
+      puts "Playing #{song}"
+    else
+      puts "Invalid input, please try again"
+    end
+    
+  end
 end
 
 def list(songs)
@@ -30,5 +43,6 @@ def list(songs)
 end
 
 def exit_jukebox
+  puts "Goodbye"
 end
 
