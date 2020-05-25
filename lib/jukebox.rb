dcdaedf863e05281e032f3cb978e20f5d62a2188
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-25 01:19:43 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,7 +17,7 @@ def help
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
-  puts ""
+  puts "- exit : exits this program"
 end
 
 def play
