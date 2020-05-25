b1238c30752193c47eabbca88f15ae58e235ab07
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-25 01:19:28 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,6 +16,7 @@ def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
   puts ""
 end
 
